7b6329db6f17ab23da8961e73f236adb2611b8ac
Max <113231278+MIXIX333@users.noreply.github.com>
2024-09-26 12:49:28 +0300

    Add files via upload

---
+++ pi.java
@@ -0,0 +1,16 @@
+
+public class pi {
+
+   
+    public static void main(String args[]) {
+        String name = "Max";
+        String lastname = "Malamo";
+        int age = 16;
+        double height = 180;
+        System.out.println("Name: " + name);
+        System.out.println("Last name: " + lastname);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is a student");     
+    }
+}
